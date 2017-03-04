6aadcd38f5691ff413698f85b738c43d6ef92066
Ikenna Nwaiwu <ikenna.nwaiwu@digital.hmrc.gov.uk>
2017-03-04 04:41:33 +0000

    Temperature Conversion

---
+++ src/main/scala/consulting/ikenna/beginningscala/TemperatureConverter.scala
@@ -0,0 +1,8 @@
+package consulting.ikenna.beginningscala
+
+object TemperatureConverter {
+  def centigradeToFahrenheit(n: Int): Double = ???
+
+  def fahrenheitToCentigrade(n: Int): Double = ???
+
+}
